6f710c095626015ac567a63994d48d83802e55a7
Sarah Bullard <scadmusbullard@gmail.com>
2019-08-01 14:06:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about earth";
+var topic = "Let's talk about space.";
 
 var conversation = `${topic}`;
 
