3b61df5d66b2a83f932c296a38cca2df1945dfd8
Sarah Bullard <scadmusbullard@gmail.com>
2019-08-01 14:22:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = "Let's talk about " + "space."
+var conversation = "Let's talk about " + topic; 
 
 
